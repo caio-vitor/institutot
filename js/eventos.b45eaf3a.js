46775a51446f9a477d7db6314b74ce3a147b6f10
caio-vitor <caio_vit@yahoo.com.br>
2019-07-09 20:45:45 -0300

    Add files via upload

---
+++ js/eventos.b45eaf3a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["eventos"],{"2a76":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("eventos")])])}],c=n("2877"),l={},r=Object(c["a"])(l,s,a,!1,null,null,null);t["default"]=r.exports}}]);
+//# sourceMappingURL=eventos.b45eaf3a.js.map
